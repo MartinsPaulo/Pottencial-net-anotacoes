dcfbb3bfdf894abc67e023a1af3842c19c6161a7
Paulo Martins <pgsm5@hotmail.coM>
2022-09-27 18:05:35 -0300

    first commit

---
+++ ExemploArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿//using ExemploArrays.Common.Models;
+
+int [] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+//arrayInteiros[3] = 1;
+
+for (int contador = 0; contador < arrayInteiros.Length; contador ++) {
+    Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+}
+
+Console.WriteLine("Percorrendo o Array com o FOREACH");
+
+foreach (int valor in arrayInteiros) 
+{
+    Console.WriteLine(valor);
+}
+
+///Array.Resize Altera o tamanho maximo do array
+// Na verdade por baixo dos panos ele cria um novo array com a capacidade que você acabou de passar
+// e copia os elementos que você criou e passa para o outro.
+//Array.Resize(ref arrayInteiros, arrayInteiros.Length*2);
+
+
+// Array.Copy (); // copia um array para outro.
---
+++ ExemploArrays/desafio2.cs
@@ -0,0 +1,25 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int qt = int.Parse(Console.ReadLine());
+		// TODO: Crie as outras condições necessárias para a resolução do desafio:
+        for (int i = 0; i < qt; i++)
+        {
+            string[] v = Console.ReadLine().Split(' ');
+			string a = v[0];
+            string b = v[1];
+
+			 if (b.Length > a.Length)
+                Console.WriteLine("nao encaixa");
+            else if (String.Equals(a.Substring(a.Length-b.Length, b.Length), b))
+                Console.WriteLine("encaixa");
+            else
+                Console.WriteLine("nao encaixa");
+		
+        }
+
+	}
+}
