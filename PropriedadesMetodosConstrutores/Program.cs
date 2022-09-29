2b09fdc9d42148ff80080ede538fb21a7cbd57da
Paulo Martins <pgsm5@hotmail.coM>
2022-09-29 15:58:58 -0300

    Fila,Pilha e Dicionarios

--- PropriedadesMetodosConstrutores/Program.cs
+++ PropriedadesMetodosConstrutores/Program.cs
@@ -1,4 +1,36 @@
 using PropriedadesMetodosConstrutores.Models;
 using System.Globalization;
 
-new ExemploExcecao().Metodo1();
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+Console.WriteLine("------------");
+estados.Remove("BA");
+estados["SP"] = "São Paulo - valor alterado";
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+string chave = "BA";
+Console.WriteLine($"Verificando chave: {chave}");
+
+if(estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: {chave}");
+}
+else
+{
+    Console.WriteLine($"Valor não existente. É seguro adicionar a chave: {chave}");
+}
+
+Console.WriteLine(estados["MG"]);
