72c48bffc4429eaddbeb9b3b52a89de63b90fa6f
Paulo Martins <pgsm5@hotmail.coM>
2022-09-29 14:26:53 -0300

    Trabalhando com datas

--- PropriedadesMetodosConstrutores/Program.cs
+++ PropriedadesMetodosConstrutores/Program.cs
@@ -17,17 +17,43 @@ cursoDeIngles.AdicionarAluno(p2);
 Console.WriteLine($"Alunos do curso de: {cursoDeIngles.Nome}");
 cursoDeIngles.ListarAlunos();
 
-//Valor monetario
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");//pt-BR e etcs
+//Formatações
+Console.WriteLine("\nFormatações:");
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");//pt-BR e etcs
 
 decimal valorMonetario = 1232.40M;
 
 Console.WriteLine($"{valorMonetario:C}");
-Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
 Console.WriteLine(valorMonetario.ToString("C1")); //c(n) n= numero de casa decimais
 
+double porcentagem = .3421;
+
+Console.WriteLine($"Porcentagem: {porcentagem.ToString("P")}");
+
+int numero = 96296042;
+Console.WriteLine($"Numero telefone: {numero.ToString("#####-####")}");
+
+//Tipo datetime é uma struct 
+
+DateTime data1 = DateTime.Now;
+Console.WriteLine("\nDateTime:");
+Console.WriteLine(data1);
+Console.WriteLine(data1.ToString("dd/MM/yyyy HH:mm"));
+Console.WriteLine(data1.ToShortTimeString());
 
 
+string dataString = "2022-04-17 18:00";
+bool sucesso = DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
+
+if(sucesso)
+{
+    Console.WriteLine($"Conversão com sucesso Data: {data}");
+}
+else
+{
+    Console.WriteLine($"{dataString} não é uma data válida");
+}
 
 
 
