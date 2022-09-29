796de03c27d30ee561ecfcb673c5b17785d0c026
Paulo Martins <pgsm5@hotmail.coM>
2022-09-29 15:30:58 -0300

    Lançando exceções

---
+++ PropriedadesMetodosConstrutores/Models/ExemploExcecao.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropriedadesMetodosConstrutores.Models
+{
+    public class ExemploExcecao
+    {
+        public void Metodo1()
+        {
+            try{
+                Metodo2();
+            }catch(Exception)
+            {
+                Console.WriteLine("Exceção tratada");
+            }
+        }
+
+        public void Metodo2()
+        {
+            Metodo3();
+        }
+
+        public void Metodo3()
+        {
+            Metodo4();
+        }
+
+        public void Metodo4()
+        {
+            throw new Exception("Ocorreu uma exceção");
+        }
+    }
+}
