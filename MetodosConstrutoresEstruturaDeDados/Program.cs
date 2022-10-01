e0807ea240e340cbc5c87ec1fdb3a4139366ad19
Paulo Martins <pgsm5@hotmail.coM>
2022-10-01 15:16:03 -0300

    Serializando venda em lista

--- MetodosConstrutoresEstruturaDeDados/Program.cs
+++ MetodosConstrutoresEstruturaDeDados/Program.cs
@@ -2,10 +2,26 @@ using MetodosConstrutoresEstruturaDeDados.Models;
 using System.Globalization;
 using Newtonsoft.Json;
 
-Venda v1 = new Venda(1, "Material de escritório", 25.00M);
+
+List<Venda> listaVendas = new List<Venda>();
+
+Venda v1 = new Venda(1, "Material de escritório", 35.00M);
+Venda v2 = new Venda(1, "Material de aula", 40.00M);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
 
 string serializado = JsonConvert.SerializeObject(v1);
 string serializadoIdentado = JsonConvert.SerializeObject(v1, Formatting.Indented);
 
 Console.WriteLine(serializado);
 Console.WriteLine(serializadoIdentado);
+
+/*Escrevendo um arquivo JSON*/
+
+File.WriteAllText("Arquivos/vendas.json", serializadoIdentado);
+
+/*Escrevendo uma lista de venda*/
+
+string todasVendas = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+File.WriteAllText("Arquivos/vendasListadas.json", todasVendas);
