b496d6dfe184b2c0d268e9a4acf045527c4478fb
Paulo Martins <pgsm5@hotmail.coM>
2022-10-01 15:39:51 -0300

    Atributos para serialização newsofjson

--- MetodosConstrutoresEstruturaDeDados/Models/Venda.cs
+++ MetodosConstrutoresEstruturaDeDados/Models/Venda.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MetodosConstrutoresEstruturaDeDados.Models
 {
@@ -16,6 +17,7 @@ namespace MetodosConstrutoresEstruturaDeDados.Models
         }
     
         public int Id { get; set; }
+        [JsonProperty("Nome_Produto")]
         public string Produto { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataVenda { get; set; }
