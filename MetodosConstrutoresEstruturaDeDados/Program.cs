c652663754fc2f668994acdde1ca65a9cd30e76d
Paulo Martins <pgsm5@hotmail.coM>
2022-10-01 16:02:09 -0300

    Tipo Anonimo

--- MetodosConstrutoresEstruturaDeDados/Program.cs
+++ MetodosConstrutoresEstruturaDeDados/Program.cs
@@ -2,3 +2,8 @@ using MetodosConstrutoresEstruturaDeDados.Models;
 using System.Globalization;
 using Newtonsoft.Json;
 
+var tipoAnonimo = new { Nome = "Paulo", Sobrenome = "Martins", Altura = 1.80 };
+
+Console.WriteLine("Nome: " + tipoAnonimo.Nome);
+Console.WriteLine("Sobrenome: " + tipoAnonimo.Sobrenome);
+Console.WriteLine("Altura: " + tipoAnonimo.Altura);
