dcd40ef3238b21943b1071051df10b0616c9f724
Paulo Martins <pgsm5@hotmail.coM>
2022-10-06 18:16:01 -0300

    Método de obter por nome

--- ModuloApi/Controllers/ContatoController.cs
+++ ModuloApi/Controllers/ContatoController.cs
@@ -34,6 +34,15 @@ namespace ModuloApi.Controllers
             return Ok(contato);
         }
 
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorNome (string nome)
+        {
+            var contatos = _context.Contatos.Where(x => x.Nome.Contains(nome));
+     
+            return Ok(contatos);
+        }
+
+
         [HttpPut("{id}")]
         public IActionResult Atualizar (int id, Contato contato)
         {
