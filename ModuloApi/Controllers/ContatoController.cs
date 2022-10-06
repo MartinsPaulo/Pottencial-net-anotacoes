a2058ee28997bc6e6f747a7a1b5b299da8ac8f58
Paulo Martins <pgsm5@hotmail.coM>
2022-10-06 18:07:48 -0300

    Método de Update

--- ModuloApi/Controllers/ContatoController.cs
+++ ModuloApi/Controllers/ContatoController.cs
@@ -33,5 +33,23 @@ namespace ModuloApi.Controllers
                 return NotFound();
             return Ok(contato);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar (int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if(contatoBanco == null)
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
     }
 }
