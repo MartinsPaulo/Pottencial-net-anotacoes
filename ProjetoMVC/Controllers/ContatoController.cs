9d4a8b0e71a1440b7ba58a10d44c194d2cc70d11
Paulo Martins <pgsm5@hotmail.coM>
2022-10-13 11:10:27 -0300

    Pagina de listagem de contratos e método index controller

---
+++ ProjetoMVC/Controllers/ContatoController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
+
+namespace ProjetoMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
+        }
+    }
+}
